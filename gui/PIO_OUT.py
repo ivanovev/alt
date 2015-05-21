b81645a1adc6e4e6b16e3a92e7f6939124c2dd51
iev <iev@land.ru>
2015-05-21 21:57:57 +0300

    todo: async plot

--- gui/PIO_OUT.py
+++ gui/PIO_OUT.py
@@ -10,7 +10,7 @@ def alt_cmd_cb(dev, cmd, val=None):
         return '%s %s %s' % (cmd, dev['altname'], val)
 
 def get_ctrl(dev):
-    data = Data('ctrl', io_cb=lambda d,c: util_io_cb(d,c,prefix='ALT'))
+    data = Data(name='ctrl', io_cb=lambda d,c: util_io_cb(d,c,prefix='ALT'))
     data.add('pio', label='PIO', wdgt='spin', value={'min':0, 'max':0xFF, 'step':1}, cmd_cb=alt_cmd_cb, send=True)
     return data
 
