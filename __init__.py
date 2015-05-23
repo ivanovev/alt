9568fbecd3126ebcc6619bc7d7111e33d365ecb8
iev <iev@land.ru>
2015-05-23 19:55:17 +0300

    asyncio for control dlgs

--- __init__.py
+++ __init__.py
@@ -1,9 +1,9 @@
 
 from . import gui, srv, tools
 from util.columns import *
-from util.misc import app_devtypes
+from util.misc import app_devtypes, app_devdata
 
-devdata = lambda: get_devdata('ALT', get_columns([c_ip_addr, c_altname]), app_devtypes(gui))
+devdata = lambda: app_devdata('ALT', get_columns([c_ip_addr, c_altname]), app_devtypes(gui))
 
 from .tools import Byteswap, Fircalc, Fircomp
 
